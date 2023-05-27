3bf8a37de0a1a7132f06b11a97f42a7ff0cf7415
Brook <brooklinwai@gmail.com>
2023-05-27 13:15:56 +0800

    clean up for src

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 import { createRoot } from "react-dom/client";
-import './index.css';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import Weather from "./weather";
+import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Weather />
   </React.StrictMode>
 );
 
