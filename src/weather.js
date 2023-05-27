269188eb8eb9e7b4717534460e86bab63894ddc2
Brook <brooklinwai@gmail.com>
2023-05-27 13:40:15 +0800

    test run

--- src/weather.js
+++ src/weather.js
@@ -1,33 +1,35 @@
 import React from "react";
-import { MDBBtn, MDBContainer } from "mdb-react-ui-kit";
+import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
 
-function Weather() {
+export default function App() {
   return (
-    <MDBContainer fluid>
-      <div
-        className="d-flex justify-content-center align-items-center"
-        style={{ height: "100vh" }}>
-        <div className="text-center">
-          <img
-            className="mb-4"
-            src="https://mdbootstrap.com/img/logo/mdb-transparent-250px.png"
-            style={{ width: 250, height: 90 }}
-          />
-          <h5 className="mb-3">
-            Thank you for using our product. We're glad you're with us.
-          </h5>
-          <p className="mb-3">MDB Team</p>
-          <MDBBtn
-            tag="a"
-            href="https://mdbootstrap.com/docs/standard/getting-started/"
-            target="_blank"
-            role="button">
-            Start MDB tutorial
-          </MDBBtn>
-        </div>
-      </div>
-    </MDBContainer>
+    <MDBCarousel showIndicators showControls fade>
+      <MDBCarouselItem
+        className="w-100 d-block"
+        itemId={1}
+        src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg"
+        alt="...">
+        <h5>First slide label</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </MDBCarouselItem>
+
+      <MDBCarouselItem
+        className="w-100 d-block"
+        itemId={2}
+        src="https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg"
+        alt="...">
+        <h5>Second slide label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </MDBCarouselItem>
+
+      <MDBCarouselItem
+        className="w-100 d-block"
+        itemId={3}
+        src="https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg"
+        alt="...">
+        <h5>Third slide label</h5>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </MDBCarouselItem>
+    </MDBCarousel>
   );
 }
-
-export default Weather;
