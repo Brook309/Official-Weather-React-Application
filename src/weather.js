604a9d9f5575f6db54ca18b24e196e068b93b122
Brook <brooklinwai@gmail.com>
2023-05-28 19:14:51 +0800

    this is a test run on how quick the react functions report back to user

--- src/weather.js
+++ src/weather.js
@@ -1,38 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { MDBInputGroup, MDBInput, MDBIcon, MDBBtn } from "mdb-react-ui-kit";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function weather() {
+  let [cityData, newCityInfo] = useState("");
+
+  console.log(cityData)
+
+  function WebSearchConnect(event) {
+    event.preventDefault();
+    alert(`${cityData}`);
+  }
+
+  function CityDataInput(event) {
+    event.preventDefault();
+    newCityInfo(event.target.value);
+  }
+
   return (
     <div className="htmlCode">
       {/*Search Bar*/}
-      <MDBInputGroup>
-        <MDBInput label="Search" />
+      <MDBInputGroup onSubmit={WebSearchConnect}>
+        <MDBInput label="Enter City Name" onClick={CityDataInput} />
         <MDBBtn rippleColor="dark">
           <MDBIcon icon="search" />
         </MDBBtn>
       </MDBInputGroup>
       {/*Search Bar*/}
       {/*premade city search*/}
-      <div class="premade-city-search">
+      <div className="premade-city-search">
         <a
-          class="city-name-one name-adjust"
+          className="city-name-one name-adjust"
           href="/"
           onClick={null}
           id="TokyoSearch">
           Tokyo
         </a>
-        <div class="line-breaker">|</div>
+        <div className="line-breaker">|</div>
         <a
-          class="city-name-two name-adjust"
+          className="city-name-two name-adjust"
           href="/"
           onClick={null}
           id="MelbSearch">
           Melbourne
         </a>
-        <div class="line-breaker">|</div>
+        <div className="line-breaker">|</div>
         <a
-          class="city-name-three name-adjust"
+          className="city-name-three name-adjust"
           href="/"
           onClick={null}
           id="HonoluluSearch">
@@ -41,41 +55,43 @@ function weather() {
       </div>
       {/* premade city search end*/}
       {/* main display panel */}
-      <div class="main-display-panel">
-        <h1 class="main-diplay-city-title" id="main-title-display-search-city">
+      <div className="main-display-panel">
+        <h1
+          className="main-diplay-city-title"
+          id="main-title-display-search-city">
           Loading
         </h1>
-        <div class="icon-weather">
+        <div className="icon-weather">
           <img
             src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
             alt="weather icon"
-            class="main-display-weather-icon"
+            className="main-display-weather-icon"
             id="icon-main-display"
           />
         </div>
-        <ol class="the-upper-part-of-the-main-display-panel">
-          <li class="side-panel alt-upper"></li>
-          <li class="clock-calander alt-upper">
+        <ol className="the-upper-part-of-the-main-display-panel">
+          <li className="side-panel alt-upper"></li>
+          <li className="clock-calander alt-upper">
             <span id="theWeekDayDisplay">Monday</span> |
             <span id="theTimeHourDisplay"> 00</span>:
             <span id="theTimeMinDisplay">0</span>
             <span id="the-am-pm">AM</span>
           </li>
-          <li class="main-temp-panel alt-upper">
+          <li className="main-temp-panel alt-upper">
             <span id="Main-display-temp">0</span>
-            <span class="Celeus">
+            <span className="Celeus">
               <a
-                class="Celeus-change-colour plus"
+                className="Celeus-change-colour plus"
                 href="/"
                 onClick={null}
                 id="Celeus-link">
                 °C
               </a>
             </span>
-            <span class="Celeus-farinhite-line-breaker">|</span>
-            <span class="farinhite">
+            <span className="Celeus-farinhite-line-breaker">|</span>
+            <span className="farinhite">
               <a
-                class="Farinhite-change-colour"
+                className="Farinhite-change-colour"
                 href="/"
                 onClick={null}
                 id="Farinhite-link">
@@ -84,15 +100,15 @@ function weather() {
             </span>
           </li>
         </ol>
-        <ol class="the-lower-part-of-the-main-display-panel">
-          <li class="the-precipition alt-lower">
+        <ol className="the-lower-part-of-the-main-display-panel">
+          <li className="the-precipition alt-lower">
             Humidity: <span id="the-main-precipition">0</span>%
           </li>
-          <li class="the-wind alt-lower">
+          <li className="the-wind alt-lower">
             Wind: <span id="the-main-wind-speed">0</span> km/h
           </li>
         </ol>
-        <div class="the-description" id="main-display-weather-description">
+        <div className="the-description" id="main-display-weather-description">
           Loading
         </div>
       </div>
